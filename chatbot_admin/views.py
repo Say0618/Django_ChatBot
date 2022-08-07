d873b8520486de9b74277961a62b2885127d93ea
smartman <say0618@outlook.com>
2022-08-07 11:04:20 +0900

    second

--- chatbot_admin/views.py
+++ chatbot_admin/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+# from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
+from django.conf import settings
+
+@login_required
+def index(request):
+    print('fdefdfdfdfdf', request.user.is_authenticated)
+    if request.user.is_authenticated:
+        return render(request, 'index.html')
+    else:
+        return render(request, 'registration/login.html')
+
+# @csrf_protect
+# def login(request):
+#     username = request.GET['username']
+#     password = request.GET['password']
+
+#     user = authenticate(request, username=username, password=password)
+#     print("----------------------------user is -------------------------------",password)
+#     if user is not None:
+#         login(request, user)
+#         return redirect(settings.LOGIN_REDIRECT_URL)
+#     else:
+#         return render(request, 'registration/login.html')    
--- chatbot_web_version/settings.py
+++ chatbot_web_version/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = 'login'
+
+STATIC_URL = '/static/'
+BASE_URL = 'http://127.0.0.1:8000'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "chatbot_web_version/static"),
+)
