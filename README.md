d20b5870c060f167ad348ebcaeda5d563fcb772a
smartman <say0618@outlook.com>
2022-08-15 02:47:25 +0900

    ds

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Django_ChatBot
 Django Chatbot application
+
+
+SSH Port 7822
+Hostname server.risk-predict.com
+SSH Username root
+Default SSH Password 5c2839e46517809d
+IP Address 185.143.45.33
--- chatbot_admin/views.py
+++ chatbot_admin/views.py
@@ -612,7 +612,7 @@ def databaseDownload(request):
 
 @login_required
 def write_sheet(request):
-    read_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "\\Media\\write_sheets\\Write.xlsx"
+    read_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/Media/write_sheets/Write.xlsx"
 
     if os.path.exists(read_path):
         wb = openpyxl.load_workbook(read_path)
