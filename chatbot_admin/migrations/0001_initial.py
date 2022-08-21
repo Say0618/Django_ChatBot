b3d9615af1897fabd1071cd3451b90f63b2fba58
smartman <say0618@outlook.com>
2022-08-21 23:31:19 +0900

    111

--- chatbot_admin/migrations/0001_initial.py
+++ chatbot_admin/migrations/0001_initial.py
@@ -11,56 +11,56 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        # migrations.CreateModel(
-        #     name='Database_Excel',
-        #     fields=[
-        #         ('id', models.AutoField(primary_key=True, serialize=False)),
-        #         ('name', models.CharField(max_length=255)),
-        #         ('status', models.IntegerField(default=1)),
-        #         ('upload_date', models.DateTimeField(auto_now_add=True)),
-        #         ('file', models.FileField(upload_to='attachments/database')),
-        #     ],
-        # ),
-        # migrations.CreateModel(
-        #     name='Images_Bot',
-        #     fields=[
-        #         ('id', models.AutoField(primary_key=True, serialize=False)),
-        #         ('name', models.CharField(max_length=255)),
-        #         ('status', models.IntegerField(default=1)),
-        #         ('upload_date', models.DateTimeField(auto_now_add=True)),
-        #         ('file', models.FileField(upload_to='attachments/images')),
-        #     ],
-        # ),
-        # migrations.CreateModel(
-        #     name='InterpretationSheet',
-        #     fields=[
-        #         ('id', models.AutoField(primary_key=True, serialize=False)),
-        #         ('name', models.CharField(max_length=255)),
-        #         ('status', models.IntegerField(default=1)),
-        #         ('upload_date', models.DateTimeField(auto_now_add=True)),
-        #         ('file', models.FileField(upload_to='interpretation_sheets')),
-        #     ],
-        # ),
-        # migrations.CreateModel(
-        #     name='MasterSheet',
-        #     fields=[
-        #         ('id', models.AutoField(primary_key=True, serialize=False)),
-        #         ('name', models.CharField(max_length=255)),
-        #         ('status', models.IntegerField(default=1)),
-        #         ('upload_date', models.DateTimeField(auto_now_add=True)),
-        #         ('file', models.FileField(upload_to='master_sheets')),
-        #     ],
-        # ),
-        # migrations.CreateModel(
-        #     name='ReadSheet',
-        #     fields=[
-        #         ('id', models.AutoField(primary_key=True, serialize=False)),
-        #         ('name', models.CharField(max_length=255)),
-        #         ('status', models.IntegerField(default=1)),
-        #         ('upload_date', models.DateTimeField(auto_now_add=True)),
-        #         ('file', models.FileField(upload_to='read_sheets')),
-        #     ],
-        # ),
+        migrations.CreateModel(
+            name='Database_Excel',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False)),
+                ('name', models.CharField(max_length=255)),
+                ('status', models.IntegerField(default=1)),
+                ('upload_date', models.DateTimeField(auto_now_add=True)),
+                ('file', models.FileField(upload_to='attachments/database')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Images_Bot',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False)),
+                ('name', models.CharField(max_length=255)),
+                ('status', models.IntegerField(default=1)),
+                ('upload_date', models.DateTimeField(auto_now_add=True)),
+                ('file', models.FileField(upload_to='attachments/images')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='InterpretationSheet',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False)),
+                ('name', models.CharField(max_length=255)),
+                ('status', models.IntegerField(default=1)),
+                ('upload_date', models.DateTimeField(auto_now_add=True)),
+                ('file', models.FileField(upload_to='interpretation_sheets')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='MasterSheet',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False)),
+                ('name', models.CharField(max_length=255)),
+                ('status', models.IntegerField(default=1)),
+                ('upload_date', models.DateTimeField(auto_now_add=True)),
+                ('file', models.FileField(upload_to='master_sheets')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='ReadSheet',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False)),
+                ('name', models.CharField(max_length=255)),
+                ('status', models.IntegerField(default=1)),
+                ('upload_date', models.DateTimeField(auto_now_add=True)),
+                ('file', models.FileField(upload_to='read_sheets')),
+            ],
+        ),
         migrations.CreateModel(
             name='Settings',
             fields=[
