a44c7a4b790f8fa490c1ef6ef9d7dd386c9ff3c2
smartman <say0618@outlook.com>
2022-09-03 08:22:46 +0900

    11

--- chatbot_admin/views.py
+++ chatbot_admin/views.py
@@ -359,19 +359,24 @@ def masterSheetDownload(request):
             files = MasterSheet.objects.filter(id__in=ids)
 
             paths = []
+            names = []
             for file in files:
                 prefix = os.getcwd() + '/media/master_sheets/'
                 path = prefix + file.filename()
                 paths.append(path)
+                name = file.name
+                names.append(name)
             
             print('path is ...', paths)
 
             if len(ids) == 1:
                 file_path = paths[0]
+                file_name = names[0]
                 if os.path.exists(file_path):
                     with open(file_path, 'rb') as fh:
                         response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
-                        response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+                        # response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+                        response['Content-Disposition'] = 'inline; filename=' + file_name
                         return response
             else: 
                 zip_filename = "master.zip"
@@ -525,19 +530,24 @@ def readSheetDownload(request):
             files = ReadSheet.objects.filter(id__in=ids)
 
             paths = []
+            names = []
             for file in files:
                 prefix = os.getcwd() + '/media/read_sheets/'
                 path = prefix + file.filename()
                 paths.append(path)
+                name = file.name
+                names.append(name)
             
             print('path is ...', paths)
 
             if len(ids) == 1:
                 file_path = paths[0]
+                file_name = names[0]
                 if os.path.exists(file_path):
                     with open(file_path, 'rb') as fh:
                         response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
-                        response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+                        # response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+                        response['Content-Disposition'] = 'inline; filename=' + file_name
                         return response
             else:
                 zip_filename = "read.zip"
